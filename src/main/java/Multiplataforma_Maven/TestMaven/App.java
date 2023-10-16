76d9542b1670ec19c403dd4d21d2fa99567a2328
useralej <mauricioalej@hotmail.com>
2023-10-16 01:11:27 -0300

    primer commit desde desktop

---
+++ src/main/java/Multiplataforma_Maven/TestMaven/App.java
@@ -0,0 +1,13 @@
+package Multiplataforma_Maven.TestMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Prueba Evaluación 1 de Automatización empleando Maven" );
+    }
+}
